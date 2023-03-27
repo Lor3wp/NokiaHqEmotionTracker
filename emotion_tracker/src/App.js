145a80cceb32c28e7e9aed7a97901a34350f5c75
karoliam <kmultas@gmail.com>
2023-03-27 15:47:32 +0300

    added conditional rendering when clicking a button

--- emotion_tracker/src/App.js
+++ emotion_tracker/src/App.js
@@ -2,8 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import EmotionButton from './EmotionButtons';
+import EmotionButtonView from "./EmotionButtonView";
+import TestView from "./TestView";
+
 
 function App() {
+  
   return (
     <div className="App">
       <link
@@ -11,19 +15,7 @@ function App() {
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
       />
       <header className="App-header">
-      <h1>
-        How are you today?
-      </h1>
-        <div
-          className="emotions"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-        <EmotionButton />
-          </div>
+        <EmotionButtonView />
       </header>
     </div>
   );
