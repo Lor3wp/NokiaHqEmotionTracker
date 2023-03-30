faa1260be3e01046cd0438c10fbf9812c344fdbc
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-03-30 13:13:30 +0300

    main test

--- emotion_tracker/src/tests/EmotionsStats.test.js
+++ emotion_tracker/src/tests/EmotionsStats.test.js
@@ -1,17 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import EmotionStats from "../EmotionStats";
 
-test("renders the count of emotion data", async () => {
-  const fakeData = { count: 10 };
-  jest.spyOn(global, "fetch").mockResolvedValue({
-    json: jest.fn().mockResolvedValue(fakeData),
-  });
+test("displays the emotion stats correctly", async () => {
+  const statsData = { count: 100 };
+  const statsTodayData = { count: 10 };
+  render(
+    <EmotionStats
+      statsData={statsData}
+      setStatsData={() => {}}
+      statsTodayData={statsTodayData}
+      setStatsTodayData={() => {}}
+    />
+  );
 
-  render(<EmotionStats />);
-
-  const loadingElement = screen.getByText("Loading...");
-  expect(loadingElement).toBeInTheDocument();
-
-  const countElement = await screen.findByText("todays feelings: 10");
-  expect(countElement).toBeInTheDocument();
+  await screen.findByText(/Loading/i);
+  expect(screen.getByText(/all feelings/i)).toBeInTheDocument();
 });
