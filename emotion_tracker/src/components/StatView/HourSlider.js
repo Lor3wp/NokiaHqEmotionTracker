5586f78954d555ac587d05ec6bcaa122c612c1e1
karoliam <kmultas@gmail.com>
2023-04-19 12:53:38 +0300

    connected all remaining states to the slider and added condition that the slider is rendered only when day timeunit it active

--- emotion_tracker/src/components/StatView/HourSlider.js
+++ emotion_tracker/src/components/StatView/HourSlider.js
@@ -4,17 +4,17 @@ import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 import HourRange from "./HourRange";
 
-
 const HourSlider = ({
   minHour,
   setMinHour,
   maxHour,
   setMaxHour,
   hourRange,
-  timeUnit,
+  setHourRange,
   onChange,
+  timeUnit,
 }) => {
-  const [useSingleThumb, setUseSingleThumb] = useState(false);
+  //   const [useSingleThumb, setUseSingleThumb] = useState(false);
 
   const onHourChange = (values) => {
     setMinHour(values[0]);
@@ -24,57 +24,56 @@ const HourSlider = ({
     }
   };
 
-  const [value, setValue] = useState([0,23]);
+  const [value, setValue] = useState([0, 23]);
 
   const onSingleThumbChange = (newValue) => {
-  
-      setValue(newValue);
-      if(onChange) {
-          onChange(value)
-      }
-    };
+    setValue(newValue);
+    setMaxHour(newValue);
+    if (onChange) {
+      onChange(value);
+    }
+  };
 
-  return (
-    <div className="hourRange-Slider">
-    <div className="range-hour">
-
-    {useSingleThumb ? (
-        <p className="hour-text">{value.slice("0")[1]}:00</p>
-    ) : (
-        <p className="hour-text">
-            {minHour}:00-{maxHour}:00
-          </p>
-    )}
-    <HourRange
-        useSingleThumb={useSingleThumb}
-        setUseSingleThumb={setUseSingleThumb}
-      />
-</div>
-      {useSingleThumb ? (
-        <div className="margin-lg">
-       <RangeSlider
-      className="single-thumb"
-      value={value}
-      max={23}
-      onInput={onSingleThumbChange}
-      thumbsDisabled={[true, false]}
-      rangeSlideDisabled={true}
-    />
-  </div>
-      ) : (
-        <div className="margin-lg">
-     
-          <RangeSlider
-            min={0}
-            max={23}
-            step={1}
-            value={[minHour, maxHour]}
-            onInput={onHourChange}
-          />
-    </div>
-      )}
-    </div>
-  );
+  if (timeUnit === "day") {
+    return (
+      <div className="hourRange-Slider">
+        <div className="range-hour">
+          {hourRange ? (
+            <p className="hour-text">{value.slice("0")[1]}:00</p>
+          ) : (
+            <p className="hour-text">
+              {minHour}:00-{maxHour}:00
+            </p>
+          )}
+          <HourRange hourRange={hourRange} setHourRange={setHourRange} />
+        </div>
+        {hourRange ? (
+          <div className="margin-lg">
+            <RangeSlider
+              className="single-thumb"
+              value={value}
+              max={23}
+              onInput={onSingleThumbChange}
+              thumbsDisabled={[true, false]}
+              rangeSlideDisabled={true}
+            />
+          </div>
+        ) : (
+          (
+            <div className="margin-lg">
+              <RangeSlider
+                min={0}
+                max={23}
+                step={1}
+                value={[minHour, maxHour]}
+                onInput={onHourChange}
+              />
+            </div>
+          )
+        )}
+      </div>
+    );
+  }
 };
 
 export default HourSlider;
