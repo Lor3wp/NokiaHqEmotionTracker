739216e709b903492c23e969634bf4acd9857a6e
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-05-03 13:11:31 +0300

    message

--- emotion_tracker/src/components/Emotions.js
+++ emotion_tracker/src/components/Emotions.js
@@ -9,7 +9,7 @@ const Emotions = () => {
   const [update, setUpdate] = useState(false);
   const [buttonActive, setButtonActive] = useState(null);
   const [time, setTime] = useState(0);
-  const timerTimeMs = 0;
+  const timerTimeMs = 15000;
   const [clicked, setClicked] = useState(0);
   const [selectedButton, setSelectedButton] = useState("");
 
@@ -32,7 +32,7 @@ const Emotions = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({emotion: id, subEmotion: 1}),
+        body: JSON.stringify({ emotion: id, subEmotion: 1 }),
       });
       if (!response.ok) {
         throw new Error("Error adding emotion");
@@ -44,11 +44,7 @@ const Emotions = () => {
     }
   };
 
-<<<<<<< HEAD
   const buttonClicked = async (id, e) => {
-=======
-  const buttonClicked = async (id, e, label) => {
->>>>>>> 83007bb3880878506c2e2866799c3c8737192dd1
     addEmotion(id);
     setClicked(id);
     timerStart(e, setButtonActive);
@@ -57,17 +53,12 @@ const Emotions = () => {
 
   return (
     <div className="content">
-<<<<<<< HEAD
       <EmotionButtons
         buttonActive={buttonActive}
         clicked={clicked}
         buttonClicked={buttonClicked}
       ></EmotionButtons>
       <div style={{ visibility: buttonActive ? "hidden" : "visible" }}>
-=======
-      <EmotionButtons buttonActive={buttonActive} clicked={clicked} buttonClicked={buttonClicked} selectedButton={selectedButton} setSelectedButton={setSelectedButton}></EmotionButtons>
-      <div style={{visibility: buttonActive ? "hidden" : "visible"}}>
->>>>>>> 83007bb3880878506c2e2866799c3c8737192dd1
         <p className="infoText">
           Share your feelings again in {Math.floor(time / 1000 / 60)} mins,{" "}
           {Math.floor((time / 1000) % 60)} secs
@@ -76,12 +67,7 @@ const Emotions = () => {
       <EmotionStats update={update} />
     </div>
   );
-<<<<<<< HEAD
-};
-=======
-
 }
->>>>>>> 83007bb3880878506c2e2866799c3c8737192dd1
 export default Emotions;
 // build test
 
